64a405de69dcaf0cb7e096d68696016342deacf1
goldmund <matthewtzito@gmail.com>
2021-09-21 22:08:19 -0700

    chore: add base stylelint config

---
+++ stylelint.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-idiomatic-order',
+    'stylelint-config-recommended-scss'
+  ],
+  plugins: [
+    'stylelint-order',
+    'stylelint-scss',
+    'stylelint-images',
+    'stylelint-declaration-block-no-ignored-properties'
+  ],
+  rules: {
+    'no-empty-source': null,
+    'images/broken': true,
+    'images/prefer-data-uri': 256,
+    'plugin/declaration-block-no-ignored-properties': true
+  },
+  ignoreFiles: [
+    './coverage/**/*',
+    './dist/**/*',
+    './node_modules/**/*',
+    './src/**/__snapshots__/**/*'
+  ],
+  reportNeedlessDisables: true,
+  reportInvalidScopeDisables: true
+};
