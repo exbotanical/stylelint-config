9b8b85012897f427ce359884064d5abbe62c041b
goldmund <matthewtzito@gmail.com>
2021-10-11 16:26:56 -0700

    feat: use tabs in lieu of spaces

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,27 +1,28 @@
 module.exports = {
   extends: [
-    'stylelint-config-standard',
-    'stylelint-config-idiomatic-order',
-    'stylelint-config-recommended-scss'
+    "stylelint-config-standard",
+    "stylelint-config-idiomatic-order",
+    "stylelint-config-recommended-scss",
   ],
   plugins: [
-    'stylelint-order',
-    'stylelint-scss',
-    'stylelint-images',
-    'stylelint-declaration-block-no-ignored-properties'
+    "stylelint-order",
+    "stylelint-scss",
+    "stylelint-images",
+    "stylelint-declaration-block-no-ignored-properties",
   ],
   rules: {
-    'no-empty-source': null,
-    'images/broken': true,
-    'images/prefer-data-uri': 256,
-    'plugin/declaration-block-no-ignored-properties': true
+    "no-empty-source": null,
+    "images/broken": true,
+    "images/prefer-data-uri": 256,
+    "plugin/declaration-block-no-ignored-properties": true,
+    indentation: "tab",
   },
   ignoreFiles: [
-    './coverage/**/*',
-    './dist/**/*',
-    './node_modules/**/*',
-    './src/**/__snapshots__/**/*'
+    "./coverage/**/*",
+    "./dist/**/*",
+    "./node_modules/**/*",
+    "./src/**/__snapshots__/**/*",
   ],
   reportNeedlessDisables: true,
-  reportInvalidScopeDisables: true
+  reportInvalidScopeDisables: true,
 };
