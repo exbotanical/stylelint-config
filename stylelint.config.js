818bf0e9f86919fb8735455c92f9269475512c10
Matthew Zito <matthewtzito@gmail.com>
2021-12-13 10:05:10 -0800

    build: update deps, add lint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,28 +1,28 @@
 module.exports = {
-  extends: [
-    "stylelint-config-standard",
-    "stylelint-config-idiomatic-order",
-    "stylelint-config-recommended-scss",
-  ],
-  plugins: [
-    "stylelint-order",
-    "stylelint-scss",
-    "stylelint-images",
-    "stylelint-declaration-block-no-ignored-properties",
-  ],
-  rules: {
-    "no-empty-source": null,
-    "images/broken": true,
-    "images/prefer-data-uri": 256,
-    "plugin/declaration-block-no-ignored-properties": true,
-    indentation: "tab",
-  },
-  ignoreFiles: [
-    "./coverage/**/*",
-    "./dist/**/*",
-    "./node_modules/**/*",
-    "./src/**/__snapshots__/**/*",
-  ],
-  reportNeedlessDisables: true,
-  reportInvalidScopeDisables: true,
+	extends: [
+		'stylelint-config-standard',
+		'stylelint-config-idiomatic-order',
+		'stylelint-config-recommended-scss'
+	],
+	ignoreFiles: [
+		'./coverage/**/*',
+		'./dist/**/*',
+		'./node_modules/**/*',
+		'./src/**/__snapshots__/**/*'
+	],
+	plugins: [
+		'stylelint-order',
+		'stylelint-scss',
+		'stylelint-images',
+		'stylelint-declaration-block-no-ignored-properties'
+	],
+	reportInvalidScopeDisables: true,
+	reportNeedlessDisables: true,
+	rules: {
+		'images/broken': true,
+		'images/prefer-data-uri': 256,
+		'indentation': 'tab',
+		'no-empty-source': null,
+		'plugin/declaration-block-no-ignored-properties': true
+	}
 };
