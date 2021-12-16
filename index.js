9c1a0cfd997ba5b3928bc13d719683305570d1bf
Matthew Zito <matthewtzito@gmail.com>
2021-12-15 21:50:00 -0800

    feat: upgrae to stylelint@14

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-module.exports = require('./stylelint.config');
+module.exports = {
+	extends: [
+		'stylelint-config-idiomatic-order',
+		'stylelint-config-recommended-scss'
+	],
+	ignoreFiles: [
+		'./coverage/**/*',
+		'./dist/**/*',
+		'./node_modules/**/*',
+		'./src/**/__snapshots__/**/*'
+	],
+	overrides: [
+		{
+			customSyntax: 'postcss-html',
+			files: ['**/*.vue']
+		},
+		{
+			customSyntax: '@stylelint/postcss-css-in-js',
+			files: ['**/*.{jsx,tsx}']
+		}
+	],
+	plugins: [
+		'stylelint-order',
+		'stylelint-scss',
+		'stylelint-images',
+		'stylelint-declaration-block-no-ignored-properties'
+	],
+	reportInvalidScopeDisables: true,
+	reportNeedlessDisables: true,
+	rules: {
+		'images/broken': true,
+		'images/prefer-data-uri': 256,
+		'indentation': 'tab',
+		'no-empty-source': null,
+		'plugin/declaration-block-no-ignored-properties': true,
+		'scss/at-rule-no-unknown': [
+			true,
+			{
+				ignoreAtRules: ['layer']
+			}
+		]
+	}
+};
