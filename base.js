d3298becb7bd278c5d1f7de1a80506c2e3af83ef
Matthew Zito <matthewtzito@gmail.com>
2022-08-13 15:17:39 -0500

    chore: update reponame

--- base.js
+++ base.js
@@ -1,23 +1,23 @@
 module.exports = {
-	extends: ['stylelint-config-idiomatic-order'],
-	ignoreFiles: [
-		'./coverage/**/*',
-		'./dist/**/*',
-		'./node_modules/**/*',
-		'./src/**/__snapshots__/**/*'
-	],
-	plugins: [
-		'stylelint-order',
-		'stylelint-images',
-		'stylelint-declaration-block-no-ignored-properties'
-	],
-	reportInvalidScopeDisables: true,
-	reportNeedlessDisables: true,
-	rules: {
-		'images/broken': true,
-		'images/prefer-data-uri': 256,
-		'indentation': 'tab',
-		'no-empty-source': null,
-		'plugin/declaration-block-no-ignored-properties': true
-	}
-};
+  extends: ['stylelint-config-idiomatic-order'],
+  ignoreFiles: [
+    './coverage/**/*',
+    './dist/**/*',
+    './node_modules/**/*',
+    './src/**/__snapshots__/**/*',
+  ],
+  plugins: [
+    'stylelint-order',
+    'stylelint-images',
+    'stylelint-declaration-block-no-ignored-properties',
+  ],
+  reportInvalidScopeDisables: true,
+  reportNeedlessDisables: true,
+  rules: {
+    'images/broken': true,
+    'images/prefer-data-uri': 256,
+    'indentation': 'tab',
+    'no-empty-source': null,
+    'plugin/declaration-block-no-ignored-properties': true,
+  },
+}
--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const { extends: extensions, rules, plugins, ...rest } = require('./base');
+const { extends: extensions, rules, plugins, ...rest } = require('./base')
 
 module.exports = {
-	...rest,
-	extends: extensions.concat(['stylelint-config-recommended-scss']),
-	overrides: [
-		{
-			customSyntax: 'postcss-html',
-			files: ['**/*.vue']
-		}
-	],
-	plugins: plugins.concat(['stylelint-scss']),
-	rules: {
-		...rules,
-		'scss/at-rule-no-unknown': [
-			true,
-			{
-				ignoreAtRules: ['layer']
-			}
-		]
-	}
-};
+  ...rest,
+  extends: extensions.concat(['stylelint-config-recommended-scss']),
+  overrides: [
+    {
+      customSyntax: 'postcss-html',
+      files: ['**/*.vue'],
+    },
+  ],
+  plugins: plugins.concat(['stylelint-scss']),
+  rules: {
+    ...rules,
+    'scss/at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['layer'],
+      },
+    ],
+  },
+}
